e3e562b653ad44da62034360f6d087e66b0356c0
Oleksii Naboichenko <oleksii.naboichenko@droneup.com>
2021-10-28 23:41:14 -0400

    Fixed SPM dependencies issue

--- Package.swift
+++ Package.swift
@@ -1,30 +1,24 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "PrettyKeyboardHelper",
-    platforms: [
-        .iOS(.v13)
-    ],
-    dependencies: [
-        .package(
-            url: "https://github.com/nab0y4enko/PrettyUtils",
-            .from("0.2.1")
-        ),
-    ],
     products: [
         .library(
             name: "PrettyKeyboardHelper",
             targets: [
                 "PrettyKeyboardHelper"
             ]
-        )
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/nab0y4enko/PrettyUtils", from: "0.2.1"),
     ],
     targets: [
         .target(
             name: "PrettyKeyboardHelper",
-            dependencies: [],
             path: "PrettyKeyboardHelper",
             exclude: [
                 "PrettyKeyboardHelper.h",
--- PrettyKeyboardHelper.podspec
+++ PrettyKeyboardHelper.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name = 'PrettyKeyboardHelper'
-  s.version = '2.0.1'
+  s.version = '2.0.2'
   s.summary = 'A helper for keyboard notifications.'
   s.description = 'A safe helper for keyboard notifications written in Swift.'
   s.homepage = 'https://github.com/nab0y4enko/PrettyKeyboardHelper'
